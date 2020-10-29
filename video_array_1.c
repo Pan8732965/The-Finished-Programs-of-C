ada106b617a68f2cb5e6f7ebc3c1fe982bab4b2b
Jay Pan <62552984+Pan8732965@users.noreply.github.com>
2020-10-29 10:54:56 +0800

    add

--- video_array_1.c
+++ video_array_1.c
@@ -5,7 +5,7 @@
 int main(){
 
     //count the times of the dice count:
-    srand(time(0));
+    /*srand(time(0));
     int counter[6]={0};
     int i;
     for(i=1;i<=6000;i++){
@@ -22,6 +22,28 @@ int main(){
     int j;
     for(j=0;j<=5;j++){
         printf("%d:%d\n",j,counter[j]);
+    }*/
+    int num[10];
+    int i,enter;
+    printf("pls enter 10 numbers: ");
+    for(i=0;i<10;i++){
+        scanf("%d",&num[i]);
     }
-    return 0;
+    while(1){
+        int max_i=-1;
+        printf("Q:");
+        scanf("%d",&enter);
+        if(enter==0){
+            break;
+        }
+        for(i=0;i<10;i++){
+            if(num[i]<=enter&&(max[i]==-1||num[i]>num[max_i])){
+                max_i=i;
+            }
+        }
+        if(max_i!=-1){
+            printf("%d\n",num[max_i]);
+        }
+    }
+        return 0;
 }
